2a344cf71af04af77acfcbdb904db617ff90529d
Don Mamaril <mamarildon@gmail.com>
2014-05-06 16:38:11 -0700

    Working ChatterBox-Server. Server.js from blank.

--- server/request-handler.js
+++ server/request-handler.js
@@ -5,41 +5,110 @@
  * this file and include it in basic-server.js so that it actually works.
  * *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html. */
 
-var handleRequest = function(request, response) {
-  /* the 'request' argument comes from nodes http module. It includes info about the
-  request - such as what URL the browser is requesting. */
+// exports.handleRequest = function(request, response) {
+//   /* the 'request' argument comes from nodes http module. It includes info about the
+//   request - such as what URL the browser is requesting. */
 
-  /* Documentation for both request and response can be found at
-   * http://nodemanual.org/0.8.14/nodejs_ref_guide/http.html */
+//   /* Documentation for both request and response can be found at
+//    * http://nodemanual.org/0.8.14/nodejs_ref_guide/http.html */
 
-  console.log("Serving request type " + request.method + " for url " + request.url);
+//   console.log("Serving request type " + request.method + " for url " + request.url);
 
-  var statusCode = 200;
+//   var statusCode = 200;
 
-  /* Without this line, this server wouldn't work. See the note
-   * below about CORS. */
-  var headers = defaultCorsHeaders;
+//    Without this line, this server wouldn't work. See the note
+//    * below about CORS. 
+//   var headers = defaultCorsHeaders;
 
-  headers['Content-Type'] = "text/plain";
+//   headers['Content-Type'] = "text/plain";
 
-  /* .writeHead() tells our server what HTTP status code to send back */
-  response.writeHead(statusCode, headers);
+//   /* .writeHead() tells our server what HTTP status code to send back */
+//   response.writeHead(statusCode, headers);
 
-  /* Make sure to always call response.end() - Node will not send
-   * anything back to the client until you do. The string you pass to
-   * response.end() will be the body of the response - i.e. what shows
-   * up in the browser.*/
-  response.end("Hello, World!");
+//   /* Make sure to always call response.end() - Node will not send
+//    * anything back to the client until you do. The string you pass to
+//    * response.end() will be the body of the response - i.e. what shows
+//    * up in the browser.*/
+//   response.end("Hello, World!");
+// };
+
+//  These headers will allow Cross-Origin Resource Sharing (CORS).
+//  * This CRUCIAL code allows this server to talk to websites that
+//  * are on different domains. (Your chat client is running from a url
+//  * like file://your/chat/client/index.html, which is considered a
+//  * different domain.) 
+// var defaultCorsHeaders = {
+//   "access-control-allow-origin": "*",
+//   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+//   "access-control-allow-headers": "content-type, accept",
+//   "access-control-max-age": 10 // Seconds.
+// };
+
+// ------------------------------------
+// ------------------------------------
+// ------------------------------------
+// ------------------------------------
+
+
+
+var storage = require('./storage.js').storage;
+var url = require('url');
+var http = require("http");
+var fs = require('fs');
+
+// var responseHeaders = {
+//     'Content-Type': 'text/plain',
+//     "access-control-allow-origin": "*",
+//     "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+//     "access-control-allow-headers": "content-type, accept",
+//     "access-control-max-age": 10 // Seconds.
+// };
+
+exports.requestRouter = function (request, response) {
+  var urlObj = url.parse(request.url);
+
+  if (urlObj.path === '/1/classes/messages') {
+    if (request.method === 'OPTIONS') {
+      response.writeHead(200, {
+        'Content-Type': 'text/plain',
+        "access-control-allow-origin": "*",
+        "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "access-control-allow-headers": "content-type, accept",
+        "access-control-max-age": 10 // Seconds.
+      });
+
+      response.end();
+    }
+
+    if (request.method === 'POST') {
+      exports.messageHandler(request, response);
+    }
+
+    if (request.method === 'GET') {
+      exports.sendMessageHandler(request, response);
+    }
+  } else {
+    response.writeHead(404);
+    response.end();
+  }
 };
 
-/* These headers will allow Cross-Origin Resource Sharing (CORS).
- * This CRUCIAL code allows this server to talk to websites that
- * are on different domains. (Your chat client is running from a url
- * like file://your/chat/client/index.html, which is considered a
- * different domain.) */
-var defaultCorsHeaders = {
-  "access-control-allow-origin": "*",
-  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "access-control-allow-headers": "content-type, accept",
-  "access-control-max-age": 10 // Seconds.
+exports.messageHandler = function(request, response) {
+  response.writeHead(201, responseHeaders);
+
+  request.on('data', function(data) {
+    var stringData = data.toString();
+    storage.results.push(JSON.parse(stringData));
+  });
+
+  request.on('end', function() {
+    response.end();
+  });
+};
+
+exports.sendMessageHandler = function(request, response) {
+  responseHeaders['Content-Type'] = 'application/json';
+  response.writeHead(200, responseHeaders);
+  response.write(JSON.stringify(storage.results));
+  response.end();
 };
